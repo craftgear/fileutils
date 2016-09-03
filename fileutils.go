fea936609221a6c3c1a899a1b8cf0d62d4ea5584
Shunsuke Watanabe <craftgear@gmail.com>
2016-09-03 10:02:36 +0900

    add cp and cpDir

---
+++ fileutils.go
@@ -0,0 +1,29 @@
+package fileutils
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func cp(dest, source string, mode os.FileMode) error {
+	b, err := ioutil.ReadFile(source)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(dest, b, mode)
+}
+
+func cpDir(destDir, sourceDir string, mode os.FileMode) error {
+	return filepath.Walk(sourceDir, func(path string, info os.FileInfo, err error) error {
+		relPath, _ := filepath.Rel(sourceDir, path)
+		if relPath == "." {
+			return nil
+		}
+		if info.IsDir() {
+			return os.MkdirAll(filepath.Join(destDir, relPath), mode)
+		} else {
+			return cp(filepath.Join(destDir, relPath), path)
+		}
+	})
+}
