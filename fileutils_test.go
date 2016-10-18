3b61eb11e1e40a625dba4ccd2ecd24a0ab50ab0a
Shunsuke Watanabe <craftgear@gmail.com>
2016-10-18 20:46:15 +0900

    add some tests

--- fileutils_test.go
+++ fileutils_test.go
@@ -1,9 +1,33 @@
 package fileutils
 
-import "testing"
+import (
+	"os"
+	"os/exec"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	cmd := exec.Command("cp", "-r", "test", "_test")
+	cmd.Run()
+	m.Run()
+	cmd = exec.Command("rm", "-rf", "test")
+	cmd.Run()
+	cmd = exec.Command("mv", "_test", "test")
+	cmd.Run()
+}
 
 func TestCp(t *testing.T) {
+	dest := "./test/fuga.txt"
+	err := Cp(dest, "./test/hoge.txt", 0755)
+	if err != nil {
+		t.Error(err)
+	}
+	if _, err := os.Stat(dest); os.IsNotExist(err) {
+		t.Error(dest, "should exist", err)
+	}
 	//TODO
+	//fuga.txtの中身がhoge.txtと同じこと
+
 }
 
 func TestCpDir(t *testing.T) {
